f3f63bac294e48211e960cf227f828153cbf396f
JoyOffere <j.offere@alustudent.com>
2023-11-15 12:13:38 -0800

    All files done

---
+++ ibm-web_intro/0-main.js
@@ -0,0 +1,8 @@
+function showtime() {
+    alert("Good day, How are you doing?");
+  }
+  
+  function showtime() {
+    var now = new Date();
+    alert(now);
+  }
---
+++ ibm-web_intro/README.md
@@ -0,0 +1,2 @@
+This is a personal practice area from coursera web practice.
+Also a general README.md for the repository
